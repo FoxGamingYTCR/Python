a4053ad5999ce8676f97972cd134badaa5bf331a
FOX GAMINGYT <105371123+FoxGamingYTCR@users.noreply.github.com>
2023-03-10 11:52:39 -0500

    Https code checker
    Checks the response code (example: ERROR 404 (or simply 404)) for the website, or websites of your choice.

---
+++ https code checker.py
@@ -0,0 +1,5 @@
+import urllib.request
+import time
+print(urllib.request.urlopen("https://foxgamingyt.wixsite.com/foxgamingyt").getcode()),
+print(urllib.request.urlopen("https://foxgamingyt.wixsite.com/foxgamingyt/anti-hate").getcode()),
+time.sleep(10)
